82f7d18393f6b80f244195332d63be4afc174aed
DevDey <65980526+DevDey@users.noreply.github.com>
2023-01-12 15:29:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import requests
+import json
+
 api_url = "https://corporatebs-generator.sameerkumar.website/"
-response = requests.get(api_url)
-print(response.json())
+
+print("Need a company motto? We have the solution you've been waiting for!\n")
+CompanyName = input("Please enter your company name: ")
+print("Here are 10 mottos for " + CompanyName+ ":\n")
+
+for i in range(10):
+    FirstResponse = requests.get(api_url)
+    FirstPhrase = json.loads(FirstResponse.text)
+    SecondResponse = requests.get(api_url)
+    SecondPhrase = json.loads(SecondResponse.text)
+    print("Here at " + CompanyName + " we " + FirstPhrase["phrase"].lower() + " and " + SecondPhrase["phrase"].lower() + ".")
